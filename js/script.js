909ee5fd64e91be8feb92666545dc4a43f84e80e
victiago1011 <64992130+victiago1011@users.noreply.github.com>
2020-07-09 12:45:05 -0300

    Várias alterações efetuadas

--- js/script.js
+++ js/script.js
@@ -16,6 +16,35 @@ function enviar()
 	{
 		alert("Preencha seu Nome!")
 		nome.focus()
+		return
 	}
+	if (cidade.value=="")
+	{
+		alert("Informe a Cidade onde você mora")
+		cidade.focus()
+		return
+	}
+	if (uf.value=="")
+	{
+		alert("Informe o Estado onde você mora")
+		uf.focus()
+		return
+	}
+	if (email.value=="")
+	{
+		alert("Informe um e-mail para contato")
+		email.focus()
+		return
+	}
+	if (fone.value=="")
+	{
+		alert("Informe um Telefone para contato")
+		fone.focus()
+		return
+	}
+	
+	alert("Obrigado por nos enviar seus dados!\n\n Em breve entraremos em contato!")
+	window.location.reload();
+	
 
 }
